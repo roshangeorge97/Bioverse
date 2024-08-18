80c3d1a5f67849f6bb2ef727b52dfdc83defbe7c
Roshan George <roshangeorge2003@gmail.com>
2024-08-19 04:07:20 +0530

    frontend-changes

--- packages/next-app/app/[username]/page.tsx
+++ packages/next-app/app/[username]/page.tsx
@@ -2,21 +2,34 @@
 
 import ConnectButton from "@/components/ConnectButton";
 import Bioverse from "@/artifacts/contracts/Bioverse.sol/Bioverse.json";
-import {
-  useWeb3ModalProvider,
-  useWeb3ModalAccount,
-} from "@web3modal/ethers/react";
-import { BrowserProvider, Contract, formatUnits, ethers } from "ethers";
+import { useWeb3ModalProvider, useWeb3ModalAccount } from "@web3modal/ethers/react";
+import { BrowserProvider, Contract, ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Avvvatars from "avvvatars-react";
 import { Button } from "@/components/ui/button";
-import { Receipt } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";  // Import useRouter
 
 const page = ({ params }: { params: { username: string } }) => {
-  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { address, isConnected } = useWeb3ModalAccount();
   const { walletProvider } = useWeb3ModalProvider();
   const [usernameExists, setUsernameExists] = useState(false);
+  const [message, setMessage] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [chain, setChain] = useState("");
+  const [amount, setAmount] = useState("");
+  const router = useRouter();  // Initialize useRouter
 
   // async function getBalance() {
   //   if (!isConnected) throw Error("User disconnected");
@@ -92,6 +105,39 @@ const page = ({ params }: { params: { username: string } }) => {
     }
   }
 
+  async function handleRequestPayment() {
+    try {
+      const res = await axios.post("/api/notification", {
+        receipt: params.username,
+        senderAddress: address,
+        message,
+        currency,
+        chain,
+        amount,
+      });
+      console.log("Res Notif", res);
+    } catch (error) {
+      console.error("An error occurred while requesting payment", error);
+    }
+  }
+
+  async function handleSendPayment() {
+    try {
+      const res = await axios.post("/api/transaction", {
+        receipt: params.username,
+        senderAddress: address,
+        message,
+        currency,
+        chain,
+        amount,
+      });
+      console.log("Transaction sent", res);
+    } catch (error) {
+      console.error("An error occurred while sending payment", error);
+    }
+  }
+
+
   // const providerT: ethers.JsonRpcProvider =
   //   useEthersProvider() as ethers.JsonRpcProvider;
   // // setProvider(providerT);
@@ -114,7 +160,6 @@ const page = ({ params }: { params: { username: string } }) => {
         const response = await axios.get("/api/claim-username", {
           params: { username: params.username, address: "" },
         });
-        // Assuming the server responds with status 200 for existing users
         setUsernameExists(true);
       } catch (error) {
         if (axios.isAxiosError(error) && error.response?.status === 404) {
@@ -131,57 +176,139 @@ const page = ({ params }: { params: { username: string } }) => {
   return (
     <>
       {usernameExists ? (
-        <div className="dark relative flex min-h-screen w-full bg-gray-900">
-          {/* Your existing code */}
-          <div className="absolute top-0 right-0 m-4">
-            <ConnectButton />
-          </div>
-          <div className="w-1/5 min-h-screen bg-gray-200 flex items-center justify-center ">
-            {" "}
-            <div className="h-full w-full bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500">
-              {/* Sidebar content goes here */}
-            </div>
-            {/* Sidebar content goes here, representing the 20% width section */}{" "}
-          </div>
-          <div className="w-4/5 flex flex-col items-center justify-between my-16">
-            <div className="flex flex-col items-center justify-center space-y-10">
-              <Avvvatars value={params.username} size={180} />
-              <div className="font-bold text-gray-200 text-4xl">
-                {params.username}
-              </div>
+        <div className="flex min-h-screen bg-gray-900 text-white">
+          {/* Gradient sidebar */}
+          <div className="w-1/3 bg-gradient-to-b from-purple-600 via-pink-600 to-red-600"></div>
+
+          {/* Main content */}
+          <div className="w-2/3 p-8 flex flex-col justify-center items-center">
+            {/* "Go to Dashboard" button */}
+            <div className="absolute top-4 left-4">
+              <Button onClick={() => router.push("/dashboard")}>Go to Dashboard</Button>
             </div>
 
-            <Button
-              className="w-36 h-14 text-xl font-semibold rounded-lg"
-              onClick={async () => await reqPayments()}
-            >
-              Request Payment
-            </Button>
-            {/* Main content goes here, representing the 80% width section */}
-            {/* <button className="w-24 h-16 bg-blue-300" onClick={sendPayments}>
-            Click me
-          </button> */}
-            {/* <button onClick={getBalance}>Get User Balance</button>
-             */}
-            <div className="flex justify-between items-center space-x-32">
-              <Button
-                className="w-36 h-14 text-xl font-semibold rounded-lg"
-                onClick={async () => await sendPayments()}
-              >
-                ETC
-              </Button>
-              <Button className="w-36 h-14 text-xl font-semibold rounded-lg">
-                MATIC
-              </Button>
-              <Button className="w-36 h-14 text-xl font-semibold rounded-lg">
-                WBTC
-              </Button>
+            <div className="absolute top-4 right-4">
+              <ConnectButton />
+            </div>
+            <div className="flex flex-col items-center space-y-8 max-w-2xl w-full">
+              <Avvvatars value={params.username} size={180} />
+              <h1 className="text-4xl font-bold text-white">{params.username}</h1>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className="w-48 h-14 text-xl font-semibold rounded-lg bg-blue-600 hover:bg-blue-700 text-white">
+                    Request Payment
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Request Payment</DialogTitle>
+                    <DialogDescription>
+                      Enter the details for your payment request.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="message" className="text-right">
+                        Message
+                      </Label>
+                      <Input
+                        id="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="col-span-3"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="currency" className="text-right">
+                        Currency
+                      </Label>
+                      <Input
+                        id="currency"
+                        value={currency}
+                        onChange={(e) => setCurrency(e.target.value)}
+                        className="col-span-3"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="chain" className="text-right">
+                        Chain
+                      </Label>
+                      <Input
+                        id="chain"
+                        value={chain}
+                        onChange={(e) => setChain(e.target.value)}
+                        className="col-span-3"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="amount" className="text-right">
+                        Amount
+                      </Label>
+                      <Input
+                        id="amount"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        className="col-span-3"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <Button onClick={handleRequestPayment}>Send Request</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+              <div className="flex justify-center space-x-8 w-full">
+                {["ETC", "MATIC", "WBTC"].map((crypto) => (
+                  <Dialog key={crypto}>
+                    <DialogTrigger asChild>
+                      <Button className="w-36 h-14 text-xl font-semibold rounded-lg bg-gray-700 hover:bg-gray-600 text-white">
+                        {crypto}
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                      <DialogHeader>
+                        <DialogTitle>Send {crypto} Payment</DialogTitle>
+                        <DialogDescription>
+                          Enter the details for your {crypto} payment.
+                        </DialogDescription>
+                      </DialogHeader>
+                      <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                          <Label htmlFor="message" className="text-right">
+                            Message
+                          </Label>
+                          <Input
+                            id="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                          <Label htmlFor="amount" className="text-right">
+                            Amount
+                          </Label>
+                          <Input
+                            id="amount"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                            className="col-span-3"
+                          />
+                        </div>
+                      </div>
+                      <DialogFooter>
+                        <Button onClick={handleSendPayment}>Send Payment</Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       ) : (
-        <div className="flex items-center justify-center h-screen">
-          <div className="text-3xl font-bold text-gray-200">
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+          <div className="text-3xl font-bold text-white">
             Username does not exist
           </div>
         </div>
--- packages/next-app/app/page.tsx
+++ packages/next-app/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -10,53 +10,86 @@ import {
   useWeb3ModalProvider,
   useWeb3ModalAccount,
 } from "@web3modal/ethers/react";
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
 
 function Home() {
+  
   const { username, setUsername } = useStore();
-  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { address, isConnected } = useWeb3ModalAccount();
   const { walletProvider } = useWeb3ModalProvider();
+  const router = useRouter();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
   };
 
   const handleSubmit = async () => {
+  
     try {
       if (address) {
-        console.log("Wallet connected");
+        const response = await axios.post("/api/claim-username", {
+          username,
+          address,
+        });
+        
+        setUsername(response.data.username);
+        console.log("Username claimed successfully");
+  
+        // Redirect to /username after successful submission
+        router.push(`/${username}`);
       } else {
-        console.log("connect to a wallet and claim username on that");
+        console.log("Connect to a wallet to claim username");
       }
-
-      const response = await axios.post("/api/claim-username", {
-        username,
-        address,
-      });
-      setUsername(response.data.username);
-      console.log("Username claimed successfully");
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.error(error.response?.data.error || "An error occurred");
-      } else {
-        console.error("An unexpected error occurred:", error);
-      }
+      console.error("An error occurred:", error);
     }
   };
 
   return (
-    <>
-      <ConnectButton />
-      <div>Landing Page</div>
-      <div className="w-36">
-        <Input
-          type="text"
-          placeholder="/username"
-          value={username}
-          onChange={handleInputChange}
-        />
-        <Button onClick={handleSubmit}>Claim</Button>
-      </div>
-    </>
+    <div className="min-h-screen bg-gray-900 text-white">
+      <header className="p-4 flex justify-between items-center">
+        <div className="pl-12 text-2xl font-bold">Bioverse</div>
+        <ConnectButton />
+      </header>
+
+      <main className="container mx-auto mt-20 flex">
+        <div className="w-1/2 p-24 pl-48 pr-12">
+          <h1 className="text-5xl font-bold mb-4">On chain payments done right.</h1>
+          <p className="text-xl text-gray-400 mb-8">
+            Powered by the Ethereum network, Bioverse is your one-stop gateway for receiving payments in crypto.
+          </p>
+          <div className="flex mb-4">
+  <div className="bg-gray-800 rounded-l-lg px-4 py-3 text-gray-400 flex items-center border border-r-0 border-gray-700">
+    bioverse.com/
+  </div>
+  <Input
+  type="text"
+  placeholder="username"
+  value={username}
+  onChange={handleInputChange}
+  className="rounded-l-none rounded-r-lg bg-gray-800 border-gray-700 focus:ring-blue-500 focus:border-blue-500 flex-grow h-18"
+/>
+
+</div>
+<Button 
+  onClick={handleSubmit} 
+  className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 rounded-lg h-12"
+>
+  Get Started
+</Button>
+        </div>
+        <div className="w-1/2 p-12 pl-0 pt-12 pb-0 items-center justify-center">
+          <div className="relative w-full max-w-md">
+            <Image src="/undraw_ether_re_y7ft.svg" alt="my profile" width="350" height="350" />
+            {/* Optional: Add decorative elements or additional styling */}
+            <div className="absolute -top-4 -left-4 w-24 h-24 bg-blue-500 opacity-20 rounded-full blur-xl"></div>
+            <div className="absolute -bottom-4 -right-4 w-32 h-32 bg-purple-500 opacity-20 rounded-full blur-xl"></div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 
